ede1db3aa8c64f6d4a99bcab0e2f519736c897cf
Andi <largo@users.noreply.github.com>
2024-04-15 17:33:28 +0900

    new version of ruby-app.wasm. Fix symbol error

--- require/main.rb
+++ require/main.rb
@@ -13,3 +13,24 @@ $0 = File::basename(app_path, ".rb") if app_path
 
 JS.global.document.getElementById("spinner").style.display = "none"
 #JS.global.document.querySelector(".buttons").style.display = "block"
+
+ # Handle video file selection and setting the video source
+ video_file_element = JS.global.document.getElementById('videoFile')
+ video_file_element.addEventListener('change') do |event|
+   file = event.target.files[0]
+   if file
+     url = JS.global.URL.createObjectURL(file)
+     video_player = JS.global.document.getElementById('videoPlayer')
+     video_player.src = url
+   end
+ end
+
+ # Handle playback rate adjustments from speed buttons
+ speed_buttons = JS.global.document.querySelectorAll('.speedButton')
+ speed_buttons.to_a.each do |button|
+   button.addEventListener('click') do
+     speed = button.getAttribute('data-speed')
+     video_player = JS.global.document.getElementById('videoPlayer')
+     video_player.playbackRate = speed.to_f
+   end
+ end
