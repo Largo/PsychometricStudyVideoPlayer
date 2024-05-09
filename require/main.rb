01c8be593be5c06e1bdf8140c4e4266c082eb46c
Andi <largo@users.noreply.github.com>
2024-05-09 22:27:45 +0900

    fix miss

--- require/main.rb
+++ require/main.rb
@@ -2,7 +2,7 @@ require "js"
 require "js/require_remote"
 
 module Kernel
-  alias_method original_require_relative require_relative
+  alias_method :original_require_relative, :require_relative
   def require_relative(path)
     JS::RequireRemote.instance.load(path)
   end
